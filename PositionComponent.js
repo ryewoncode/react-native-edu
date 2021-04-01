2a9e50350932cc9d1d6e1a6a1598742141831b1f
김례원 <kky0919@reappay.co.kr>
2021-04-01 16:02:11 +0900

    feat: 컴포넌트 포지션 지정 실습

--- PositionComponent.js
+++ PositionComponent.js
@@ -1,28 +1,35 @@
 import React, { Component } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, Button } from 'react-native';
 
 
-const PositionComponent =()=>{
-  return (
-    <View style={styles.all}>
-      <View style={styles.container1}>
-          <Text style={styles.box}>Box1</Text>
-          <Text style={styles.box}>Box2</Text>
-          <Text style={styles.box}>Box3</Text>
-          <Text style={styles.box}>Box4</Text>
-      </View>
-      <View style={styles.container2}><Text>Box2</Text></View>
-      {/* <View style={styles.container3}><Text>Box3</Text></View> */}
-    </View>
-  )
-}
+const PositionComponent = () => {
+    const onBtn = () => {
+        alert('onBtn');
+    };
+    return (
+        <View style={styles.all}>
+        <View style={styles.container1}>
+            <Text style={styles.box}>Box1</Text>
+            <Text style={styles.box}>Box2</Text>
+            <Text style={styles.box}>Box3</Text>
+            <Text style={styles.box}>Box4</Text>
+            <Button title="btn"
+                    // onPress={() => {alert('btn clicked!')}} 
+                    onPress={onBtn}
+            />
+        </View>
+        <View style={styles.container2}><Text>Box2</Text></View>
+        {/* <View style={styles.container3}><Text>Box3</Text></View> */}
+        </View>
+    );
+};
 
 const styles = StyleSheet.create({
     box: {
-        width: 50,
-        height: 50,
+        width: 30,
+        height: 30,
         backgroundColor: 'red',
-        margin: 20,
+        margin: 5,
     },
   all:{
     backgroundColor: '#cccccc',
@@ -32,15 +39,15 @@ const styles = StyleSheet.create({
   container1: {
     backgroundColor: 'violet',
     flex: 1,
-    justifyContent: 'space-evenly',
-    alignItems: 'center',
+    justifyContent: 'center',
+    alignItems: 'stretch',
     padding: 10,
     // width: 150,
     // height: 150,
   },
   container2: {
     backgroundColor: 'cyan',
-    flex: 2,
+    flex: 1,
     // width: 150,
     // height: 150,
   },
