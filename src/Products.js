a2d199a20a9b800e4acb03bc113f593342ccba6a
김례원 <kky0919@reappay.co.kr>
2021-04-02 14:43:26 +0900

    feat: redux를 사용하여 데이터 추가하기

--- src/Products.js
+++ src/Products.js
@@ -1,20 +1,45 @@
-import React from 'react';
-import { View, Text } from "react-native";
-import { useSelector } from 'react-redux';
+import React, { useState } from 'react';
+import { View, Text, TextInput, Button, StyleSheet } from "react-native";
+import { useDispatch, useSelector } from 'react-redux';
+
+import { addProduct } from './action';
 
 const Products = () => {
     const products = useSelector(state => state.productReducer.products);
+    const [name, setName] = useState('');
+    const [maker, setMaker] = useState('');
+    const dispatch = useDispatch();
+
+    const addBtn = () => {
+        dispatch(addProduct({name, maker}));
+    }
+
     return (
         <View>
             <Text>Products</Text>
             <Text>{products.length}</Text>
-            {products && products.map(product => (
-                <View>
-                    <Text>{product.name}</Text>
+            {products && products.map((product, index) => (
+                <View key={index}>
+                    <Text>{product.name}, {product.maker}</Text>
                 </View>
             ))}
+            <View style={styles.addform}>
+                <TextInput style={styles.input} onChangeText={setName} placeholder="name" />
+                <TextInput style={styles.input} onChangeText={setMaker} placeholder="maker" />
+                <Button title="Add" onPress={addBtn} />
+            </View>
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    addform: {
+        flexDirection: 'row',
+    },
+    input: {
+        height: 40,
+        borderWidth: 1,
+    }
+})
 
 export default Products;
--- src/productReducer.js
+++ src/productReducer.js
@@ -1,16 +1,27 @@
+import { ADD_PRODUCT } from "./action";
+
 const initialState = {
-    products: [{
-        name: 'Zfilip',
-        maker: 'Samsung'
-    },
-    {
-        name: 'iPhone12',
-        maker: 'Apple'
-    }]
+    products: [
+        {
+            name: 'Zfilip',
+            maker: 'Samsung'
+        },
+        {
+            name: 'iPhone12',
+            maker: 'Apple'
+        }
+    ]
 };
 
-const productReducer = (state=initialState) => {
-    return state;
+const productReducer = (state=initialState, action) => {
+    switch(action.type) {
+        case ADD_PRODUCT:
+            return {
+                products: [...state.products, action.product]
+            }
+        default:
+            return state;
+    }
 }
 
 export default productReducer;
